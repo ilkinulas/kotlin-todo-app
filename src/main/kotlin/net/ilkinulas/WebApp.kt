c890fe697551fb11d2fb04d8d6fbcf73a5c3414d
ilkin Balkanay <ilkinulas@gmail.com>
2018-03-10 15:30:20 +0300

    kotlin exposed library integrated

--- src/main/kotlin/net/ilkinulas/WebApp.kt
+++ src/main/kotlin/net/ilkinulas/WebApp.kt
@@ -1,13 +1,17 @@
 package net.ilkinulas
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import kotliquery.*
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
 import spark.Spark.*
 
+
 val logger = LoggerFactory.getLogger("todo")
 val port = 9000
+val mapper by lazy { jacksonObjectMapper() }
 
 fun main(args: Array<String>) {
     Config.init()
@@ -19,82 +23,77 @@ fun main(args: Array<String>) {
     logger.info("Todo App is ready, listening on port $port")
     path("/todo/") {
         get("") { _, _ ->
-            //TODO pagination
-            jacksonObjectMapper().writeValueAsString(Todo.all())
+            transaction {
+                val todos = Todos.selectAll().map { rowToTodo(it) }
+                mapper.writeValueAsString(todos)
+            }
         }
 
         post("") { req, _ ->
-            val text = req.body()
-            if (text.isNullOrEmpty()) badRequest("Todo text must be non empty.")
-            val todoId = Todo(text = text).insert()
-            if (todoId == null) serverError("Can not save todo.")
-            else jacksonObjectMapper().writeValueAsString(Todo.get(todoId))
+            val todoText = req.body()
+            if (todoText.isNullOrEmpty()) badRequest("Todo text must be non empty.")
+            transaction {
+                val todoId = Todos.insert {
+                    it[text] = todoText
+                } get Todos.id
+                val todo = Todos.select { (Todos.id eq todoId) }.map { rowToTodo(it) }.first()
+                mapper.writeValueAsString(todo)
+            }
         }
 
         put(":id") { req, _ ->
-            val updateReq = jacksonObjectMapper().readTree(req.body())
-
-            val updated = Todo.update(
-                    req.params("id").toLong(),
-                    updateReq.get("text").asText(),
-                    updateReq.get("done").asBoolean())
-            if (updated == 1) jacksonObjectMapper().writeValueAsString(Todo.get(req.params("id").toLong()))
-            else serverError("Failed to update todo")
+            val updateReq = mapper.readTree(req.body())
+            transaction {
+                val updated = Todos.update({ Todos.id eq req.params("id").toInt() }) {
+                    it[text] = updateReq.get("text").asText()
+                    it[done] = updateReq.get("done").asBoolean()
+                }
+                val todo = Todos.select { Todos.id eq req.params("id").toInt() }.map { rowToTodo(it) }
+                if (updated == 1) mapper.writeValueAsString(todo)
+                else serverError("Failed to update todo")
+            }
         }
 
         delete(":id") { req, _ ->
-            val deleted = using(sessionOf(HikariCP.dataSource())) { session ->
-                session.run(queryOf("DELETE FROM TODOS WHERE ID=?", req.params("id").toLong()).asUpdate)
+            transaction {
+                val deleted = Todos.deleteWhere { Todos.id eq req.params("id").toInt() }
+                if (deleted == 1) "ok"
+                else serverError("Failed to delete todo ${req.params("id")}")
             }
-
-            if (deleted == 1) "ok"
-            else serverError("Failed to delete todo ${req.params("id")}")
         }
     }
 }
+
+private fun rowToTodo(it: ResultRow) =
+        Todo(it[Todos.id], it[Todos.text], it[Todos.done], it[Todos.dateCreated])
 
 fun badRequest(reason: String) = halt(400, reason)
 fun serverError(reason: String) = halt(500, reason)
 
-data class Todo(val id: Long = 0, val text: String, val done: Boolean = false, val dateCreated: DateTime = DateTime.now()) {
-    companion object {
-        fun all() = using(sessionOf(HikariCP.dataSource())) { con ->
-            con.run(queryOf("SELECT * FROM TODOS").map(rowToTodo).asList)
-        }
-
-        fun get(id: Long) = using(sessionOf(HikariCP.dataSource())) { con ->
-            con.run(queryOf("SELECT * FROM TODOS WHERE ID=?", id).map(rowToTodo).asSingle)
-        }
-
-        fun delete(id: Long) = using(sessionOf(HikariCP.dataSource())) { con ->
-            con.run(queryOf("DELETE FROM FROM TODOS WHERE ID=?", id).asUpdate)
-        }
-
-        fun update(id: Long, text: String, done: Boolean) = using(sessionOf(HikariCP.dataSource())) { con ->
-            con.run(queryOf("UPDATE TODOS SET TEXT=?, DONE=? WHERE ID=?", text, done, id).asUpdate)
-        }
-    }
+object Todos : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val text = varchar("text", 256)
+    val done = bool("done").default(false)
+    val dateCreated = date("date_created").default(DateTime.now())
 }
+
+data class Todo(val id: Int = 0, val text: String, val done: Boolean = false, val dateCreated: DateTime = DateTime.now())
 
 fun setupDatabase() {
-    HikariCP.default(Config.get("jdbc.url"), Config.get("jdbc.user"), Config.get("jdbc.password"))
-    if (Config.isInMemoryDb()) {
-        using(sessionOf(HikariCP.dataSource())) {
-            it.run(queryOf(Config.readFromClassPath("/sql/todo-h2.sql")).asExecute)
-        }
+    val ds = HikariDataSource()
+    ds.jdbcUrl = Config.get("jdbc.url")
+    ds.username = Config.get("jdbc.username")
+    ds.password = Config.get("jdbc.password")
+    Database.connect(ds)
+
+    transaction {
+        SchemaUtils.create(Todos)
+        addTestData()
     }
-    addTestData()
 }
-
-fun Todo.insert() = using(sessionOf(HikariCP.dataSource())) { con ->
-    con.run(queryOf("INSERT INTO TODOS (TEXT) VALUES (?)", this.text).asUpdateAndReturnGeneratedKey)
-}
-
-
-val rowToTodo: (Row) -> Todo = { row -> Todo(row.long("ID"), row.string("TEXT"), row.boolean("DONE"), row.jodaDateTime("DATE_CREATED")) }
 
 fun addTestData() {
-    Todo(text = "Prepare presentation for JavaDay 2018.").insert()
-    Todo(text = "Favor object composition over class inheritance.").insert()
-    Todo(text = "Automate your tests.").insert()
+    Todos.insert { it[text] = "Prepare presentation for JavaDay 2018." }
+    Todos.insert { it[text] = "Favor object composition over class inheritance." }
+    Todos.insert { it[text] = "Automate your tests." }
 }
